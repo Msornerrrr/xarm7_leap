9ded0f846f34ccdf48e1ab64f231ceb274a5c050
Hao Jiang <hjiang86@usc.edu>
2025-08-28 19:23:45 -0700

    fix bug

--- leap_utils/control_leap_exp.py
+++ leap_utils/control_leap_exp.py
@@ -1,8 +1,15 @@
 import numpy as np
 import time
 
-from leap_python import LeapNode
+from leap_python import LeapNode, leap_hand_utils as lhu
 
+
+def tmp_function(x):
+    leap_q = lhu.allegro_to_LEAPhand(x, zeros=False)
+
+    # 4) Safety-clip to real LEAP limits
+    leap_q = lhu.angle_safety_clip(leap_q)
+    return leap_q
 
 def main():
     # Initialize Leap Hand
@@ -19,20 +26,20 @@ def main():
 
     print("Starting LEAP hand test...")
     print("Moving to home position.")
-    leap_hand.set_leap(HOME_POSE)
+    leap_hand.set_leap(tmp_function(HOME_POSE))
     time.sleep(2.0)
 
     while True:
         # Close hand
         print("Closing hand...")
-        leap_hand.set_leap(CLOSED_POSE)
+        leap_hand.set_leap(tmp_function(CLOSED_POSE))
 
         time.sleep(2.0)
         print("Current pos:", leap_hand.read_pos())
 
         # Open hand
         print("Opening hand...")
-        leap_hand.set_leap(HOME_POSE)
+        leap_hand.set_leap(tmp_function(HOME_POSE))
         time.sleep(2.0)
         print("Current pos:", leap_hand.read_pos())
 
